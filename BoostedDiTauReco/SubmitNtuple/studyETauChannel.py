17ac9ebac1a34fe0722322b4631a443769b589d8
Muti <mw18h@my.fsu.edu>
2023-01-18 05:23:15 -0600

    latest code for ETau channel

--- BoostedDiTauReco/SubmitNtuple/studyETauChannel.py
+++ BoostedDiTauReco/SubmitNtuple/studyETauChannel.py
@@ -24,6 +24,8 @@ if len(sys.argv)>2 and not sys.argv[2].startswith("-"):
 else:
     outputFileDir = "./output/"
 
+outputFileName = outputFileDir+outputTitle+"_"+inputFileListName.split("/")[-1].replace(".txt",".root")
+
 out=ROOT.TFile.Open(outputFileName,'recreate')
 print(outputFileName)
 
@@ -32,6 +34,8 @@ chain2 = ROOT.TChain('tcpTrigNtuples/triggerTree')
 if isData == 0:
     chain3 = ROOT.TChain('lumiSummary/lumiTree')
     chain4 = ROOT.TChain('tcpGenNtuples/genTree')
+
+pi = np.pi
 
 h = {}
 
@@ -47,3 +51,368 @@ event_cut = {
     'mass' : 5
 }
 
+#define histograms here
+
+def book_histogram():
+
+    h['hEvents'] = ROOT.TH1F ("NEvents", "Number of Events; ;N", 2, 0, 2)
+    h['hWeights'] = ROOT.TH1F ("hWeights", "Weights per events; weight; N", 100, 0, 2)
+    h['hGenWeights'] = ROOT.TH1F ("hGenWeights", "Genweights per events; genweight; N", 100, 0, 2)
+    h['hPuWeights'] = ROOT.TH1F ("hPuWeights", "PUweights per events; PUweight; N", 100, 0, 2)
+
+    # ---------- Objects ---------- #
+
+    h['hJetPt'] = ROOT.TH1F ("hJetPt", "Jet P_{T} ; P_{T} ; N", 1500, 0, 1500)
+    h['hDeepjet'] = ROOT.TH1F ("hDeepjet", "deepjet score ; score ; N", 100, 0, 1)
+    h['hBJetPt'] = ROOT.TH1F ("hBJetPt", "BJet P_{T} ; P_{T} ; N", 1500, 0, 1500)
+
+    h['hMuonPt'] = ROOT.TH1F ("hMuPt", "Muon P_{T} ; P_{T} ; N", 500, 0, 500)
+    h['hIsoMuonPt'] = ROOT.TH1F ("hIsoMuPt", "Isolated Muon P_{T} ; P_{T} ; N", 500, 0, 500)
+    h['hNonIsoMuonPt'] = ROOT.TH1F ("hNonIsoMuPt", "Non-Isolated Muon P_{T} ; P_{T} ; N", 500, 0, 500)
+
+    h['hElectronPt'] = ROOT.TH1F ("hEPt", "Electron P_{T} ; P_{T} ; N", 500, 0, 500)
+    h['hIsoElectronPt'] = ROOT.TH1F ("hIsoEPt", "Isolated Electron P_{T} ; P_{T} ; N", 500, 0, 500)
+    h['hNonIsoElectronPt'] = ROOT.TH1F ("hNonIsoEPt", "Non-Isolated Electron P_{T} ; P_{T} ; N", 500, 0, 500)
+
+    h['hTauECleanedPt'] = ROOT.TH1F ("hTauECleanedPt", "Electron-Cleaned Tau P_{T} ; P_{T} ; N", 500, 0, 500)
+    h['hTauMuCleanedPt'] = ROOT.TH1F ("hTauMuCleanedPt", "Muon-Cleaned Tau P_{T} ; P_{T} ; N", 500, 0, 500)
+
+def book_event_histogram(region):
+
+    h[region+"_Count"] = ROOT.TH1F (region+"_Count", region+"_Count ; Events ; Events ", 1, 0, 1)
+
+    h[region+"_Mass"] = ROOT.TH1F (region+"_Mass", region+"_Mass ; M_{vis.} (GeV) ; Events ", 100, 0, 100)
+    h[region+"_Lepton1Pt"] = ROOT.TH1F (region+"_Lepton1Pt", region+"_Lepton1Pt ; P_{T} (GeV) ; Events ", 500, 0, 500)
+    h[region+"_Lepton2Pt"] = ROOT.TH1F (region+"_Lepton2Pt", region+"_Lepton2Pt ; P_{T} (GeV) ; Events ", 500, 0, 500)
+    h[region+"_JetPt"] = ROOT.TH1F (region+"_JetPt", region+"_JetPt ; JetP_{T} (GeV) ; Events ", 2000, 0, 2000)
+    h[region+"_MetPt"] = ROOT.TH1F (region+"_MetPt", region+"_MetPt ; MET (GeV) ; Events ", 500, 0, 500)
+    h[region+"_Nj"] = ROOT.TH1F (region+"_Nj", region+"_Nj ; N_{j} ; Events ", 10, 0, 10)
+    h[region+"_dRl"] = ROOT.TH1F (region+"_dRl", region+"_dRl ; dR(leptons) ; Events", 100, 0, 5)
+    h[region+"_dRj"] = ROOT.TH1F (region+"_dRj", region+"_dRj ; dR(jet, ditau) ; Events", 100, 0, 5)
+    h[region+"_dPhil"] = ROOT.TH2F (region+"_dPhil", region+"_dPhil ; dPhi(met,lepton1) ; dPhi(met,lepton2)",  100, -pi, pi, 100, -pi, pi)
+    h[region+"_dPhi"] = ROOT.TH2F (region+"_dPhi", region+"_dPhi ; dPhi(met,ditau) ; dPhi(met,jet)",  100, -pi, pi, 100, -pi, pi)
+    
+
+def get_TLorentzVector(obj):
+    
+    v = ROOT.TLorentzVector()
+    v.SetPtEtaPhiM(obj.pt, obj.eta, obj.phi, obj.mass)
+
+    return v
+
+
+def pass_deltaR(l1, l2, j, channel):
+
+    if channel == "MuTau" or channel == "ETau":
+        if l1.DeltaR(l2) < event_cut["dRl"] and j.DeltaR(l1) > event_cut["dRlj"] and j.DeltaR(l2) > event_cut["dRlj"] and l1.DeltaR(l2) > event_cut["dRltau"]:
+            return 1
+        else:
+            return -9999
+    if channel == "MuMu" or channel == "EMu" :
+        if l1.DeltaR(l2) < event_cut["dRl"] and j.DeltaR(l1) > event_cut["dRlj"] and j.DeltaR(l2) > event_cut["dRlj"]:
+            return 1
+        else:
+            return -9999
+
+def Mt(lepton, met):
+
+    cos = np.cos(met.DeltaPhi(lepton))
+    Mt = np.sqrt(2*lepton.Pt()*met.Pt()*(1-cos))
+
+    return Mt
+
+def plot_variable(region, l1, l2, j, m):
+
+    h[region+"_Count"].Fill(0, weight)
+
+    h[region+"_Mass"].Fill((l1+l2).M(), weight)
+    h[region+"_Lepton1Pt"].Fill(l1.Pt(), weight)
+    h[region+"_Lepton2Pt"].Fill(l2.Pt(), weight)
+    h[region+"_JetPt"].Fill(j.Pt(), weight)
+    h[region+"_MetPt"].Fill(m.Pt(), weight)
+    h[region+"_Nj"].Fill(len(s_jet), weight)
+    h[region+"_dRl"].Fill(l1.DeltaR(l2), weight)
+    h[region+"_dRj"].Fill(j.DeltaR(l1+l2), weight)
+    h[region+"_dPhil"].Fill(m.DeltaPhi(l1), m.DeltaPhi(l2), weight)
+    h[region+"_dPhi"].Fill(m.DeltaPhi(l1+l2), m.DeltaPhi(j), weight)
+
+
+def mumu_channel():
+
+    isMuMu = 0
+
+    if s_isomuon[0].charge*s_isomuon[1].charge < 0 :
+
+        mu1 = get_TLorentzVector(s_isomuon[0])
+        mu2 = get_TLorentzVector(s_isomuon[1])
+        jet = get_TLorentzVector(s_jet[0])
+
+        if ( mu1.Pt() > 50 and isMu == 1 ) or ( mu1.Pt() > 27 and isIsoMu == 1 )\
+        or ( jet.Pt() > 500 and isHT == 1 ) :
+
+            if pass_deltaR(mu1, mu2, jet, 'MuMu') == 1 :
+
+                plot_variable('MuMu', mu1, mu2, jet, met)
+
+                isMuMu = 1
+
+    return isMuMu
+
+
+def etau_channel():
+
+    e = get_TLorentzVector(s_electron[0])
+    tau = get_TLorentzVector(s_tauEclean[0])
+    jet = get_TLorentzVector(s_jet[0])
+
+    plot_variable('ETau', e, tau, jet, met)
+
+
+def mutau_channel():
+
+    isMuTau = 0
+
+    if s_muon[0].charge*s_tauMuclean[0].charge < 0:
+
+        mu = get_TLorentzVector(s_muon[0])
+        tau = get_TLorentzVector(s_tauMuclean[0])
+        jet = get_TLorentzVector(s_jet[0])
+
+        if ( mu.Pt() > 50 and isMu == 1 ) or ( mu.Pt() > 27 and isIsoMu == 1 )\
+        or ( jet.Pt() > 500 and isHT == 1 ) :
+
+            if pass_deltaR(mu, tau, jet, 'MuTau') == 1 :
+
+                if Mt(mu,met) < 50 :
+
+                    plot_variable('MuTau', mu, tau, jet, met)
+                    isMuTau = 1
+
+    return isMuTau
+
+
+def emu_channel():
+
+    isEMu = 0
+
+    if s_isomuon[0].charge*s_isoelectron[0].charge < 0:
+
+        e = get_TLorentzVector(s_isoelectron[0])
+        mu = get_TLorentzVector(s_isomuon[0])
+        jet = get_TLorentzVector(s_jet[0])
+
+        if ( mu.Pt() > 50 and isMu == 1 ) or ( mu.Pt() > 27 and isIsoMu == 1 )\
+        or ( ( ( mu.Pt() > 8 and e.Pt() > 23 ) or ( mu.Pt() > 23 and e.Pt() > 12 ) ) and isMuonEG == 1 )\
+        or ( jet.Pt() > 500 and isHT == 1 ) :
+
+            if pass_deltaR(e, mu, jet, 'EMu') == 1 :
+
+                if met.Pt() > event_cut['metcut'] :
+            
+                    plot_variable('EMu', e, mu, jet, met)
+                    
+                    isEMu = 1
+
+    return isEMu
+
+
+book_histogram()
+book_event_histogram('MuMu')
+book_event_histogram('ETau')
+book_event_histogram('MuTau')
+book_event_histogram('EMu')
+
+#-------- File loop --------#
+
+
+inputFileNames=open(inputFileList, 'r')
+for inputFileName in inputFileNames:
+    inputFileName=inputFileName.replace("\n","")
+    print(inputFileName.replace("\n",""))
+
+    fchain.Add(inputFileName)
+    chain2.Add(inputFileName)
+    if isData == 0:
+        chain3.Add(inputFileName)
+        chain4.Add(inputFileName)
+
+
+#------- Adding friends to the main chain -------#
+
+
+fchain.AddFriend(chain2)
+if isData == 0:
+    fchain.AddFriend(chain3)
+    fchain.AddFriend(chain4)
+
+jets = ROOT.JetInfoDS()
+muons = ROOT.MuonInfoDS()
+electrons = ROOT.ElectronInfoDS()
+tausUnCleaned = ROOT.TauInfoDS()
+tausECleaned = ROOT.TauInfoDS()
+tausMCleaned = ROOT.TauInfoDS()
+tausBoosted = ROOT.TauInfoDS()
+
+fchain.SetBranchAddress("Jets", ROOT.AddressOf(jets))
+fchain.SetBranchAddress("Muons", ROOT.AddressOf(muons))
+fchain.SetBranchAddress("Electrons", ROOT.AddressOf(electrons))
+fchain.SetBranchAddress("TausUnCleaned", ROOT.AddressOf(tausUnCleaned))
+fchain.SetBranchAddress("TausECleaned", ROOT.AddressOf(tausECleaned))
+fchain.SetBranchAddress("TausMCleaned", ROOT.AddressOf(tausMCleaned))
+fchain.SetBranchAddress("TausBoosted", ROOT.AddressOf(tausBoosted))
+
+if isData == 0:
+    genParticle = ROOT.GenParticleInfoDS()
+    fchain.SetBranchAddress("GenParticleInfo", ROOT.AddressOf(genParticle))
+
+
+#----------- Event loop ----------#
+
+
+for iev in range(fchain.GetEntries()): # Be careful!!!                                                               
+
+    fchain.GetEntry(iev)
+
+    mets = fchain.GetBranch("Mets")
+    met_pt = mets.GetLeaf('pt').GetValue()
+    met_phi = mets.GetLeaf('phi').GetValue()
+
+    met = ROOT.TLorentzVector()
+    met.SetPtEtaPhiM(met_pt, 0, met_phi, 0)
+
+    if isData == 0:
+        genweight = fchain.GetLeaf('genWeight').GetValue()
+        puweight = fchain.GetLeaf('puWeight').GetValue()
+    else :
+        genweight = 1
+        puweight = 1
+
+    weight = genweight*puweight
+
+    h['hEvents'].Fill(0.5, 1)
+    h['hEvents'].Fill(1.5, weight)
+
+    h['hWeights'].Fill(weight)
+    h['hPuWeights'].Fill(puweight)
+    h['hGenWeights'].Fill(genweight)
+
+    isSingleJet = fchain.GetLeaf('isSingleJet').GetValue()
+    isHT = fchain.GetLeaf('isHT').GetValue()
+    isHTMHT = fchain.GetLeaf('isHTMHT').GetValue()
+    isMu = fchain.GetLeaf('isMu').GetValue()
+    isIsoMu = fchain.GetLeaf('isIsoMu').GetValue()
+    isIsoMuTau = fchain.GetLeaf('isIsoMuTau').GetValue()
+    isIsoEle = fchain.GetLeaf('isIsoEle').GetValue()
+    isEleTau = fchain.GetLeaf('isEleTau').GetValue()
+    isMuonEG = fchain.GetLeaf('isMuonEG').GetValue()
+
+    #------------ Objects loop ------------#
+
+    s_jet = []
+    s_bjet = []
+
+    if jets.size() > 0:
+        for i in range(jets.size()):
+            ijet = jets.at(i)
+            if abs(ijet.eta) < 2.5 :
+                if ijet.id >= 2:
+                    h['hJetPt'].Fill(ijet.pt, weight)
+                    h['hDeepjet'].Fill(ijet.deepjet, weight)
+                    s_jet+=[ijet]
+                    if ijet.deepjet >= 0.7476:
+                        h['hBJetPt'].Fill(ijet.pt, weight)
+                        s_bjet+=[ijet]
+
+    s_muon = []
+    s_isomuon = []
+    s_nonisomuon = []
+
+    if muons.size() > 0:
+        for i in range(muons.size()):
+            imuon = muons.at(i)
+            if abs(imuon.eta) < 2.4 :
+                if imuon.id >= 1: #loose Muons
+                    h['hMuonPt'].Fill(imuon.pt, weight) 
+                    s_muon+=[imuon]
+                    if imuon.iso <= 0.25:
+                        h['hIsoMuonPt'].Fill(imuon.pt, weight)
+                        s_isomuon+=[imuon]
+                    if imuon.iso > 0.25:
+                        h['hNonIsoMuonPt'].Fill(imuon.pt, weight)
+                        s_nonisomuon+=[imuon]
+
+    s_electron = []
+    s_isoelectron = []
+    s_nonisoelectron = []
+
+    if electrons.size() > 0:
+        for i in range(electrons.size()):
+            ielectron = electrons.at(i)
+            if abs(ielectron.eta) < 2.5 :
+                if ielectron.id >= 1 :
+                    h['hElectronPt'].Fill(ielectron.pt, weight)
+                    s_electron+=[ielectron]
+                    if ielectron.iso >= 1:
+                        h['hIsoElectronPt'].Fill(ielectron.pt, weight)
+                        s_isoelectron+=[ielectron]
+                    if ielectron.iso == 0:
+                        h['hNonIsoElectronPt'].Fill(ielectron.pt, weight)
+                        s_nonisoelectron+=[ielectron]
+
+
+    s_tauEclean = []
+
+    if tausECleaned.size()>0:
+        for i in range(tausECleaned.size()):
+            itau = tausECleaned.at(i)
+            if abs(itau.eta) < 2.3 :
+                if itau.mvaid >= 4:
+                    h['hTauECleanedPt'].Fill(itau.pt, weight)
+                    s_tauEclean+=[itau]
+
+    s_tauMuclean = []
+
+    if tausMCleaned.size()>0:
+        for i in range(tausMCleaned.size()):
+            itau = tausMCleaned.at(i)
+            if abs(itau.eta) < 2.3 :
+                if itau.mvaid >= 4 :
+                    h['hTauMuCleanedPt'].Fill(itau.pt, weight)
+                    s_tauMuclean+=[itau]
+
+
+    # ---------- Event Selections --------- #
+
+    if len(s_isomuon) >= 2 and len(s_jet) >= 1 : 
+        if mumu_channel() == 1: continue
+
+    if len(s_isomuon) >= 1 and len(s_isoelectron) >= 1 and len(s_jet) >= 1 : 
+        if emu_channel() == 1 : continue
+
+    if len(s_muon) >= 1 and len(s_tauMuclean) >= 1 and len(s_jet) >= 1 : 
+        if mutau_channel() == 1 : continue
+
+    if len(s_electron) >= 1 and len(s_tauEclean) >= 1 and len(s_jet) >= 1 and s_electron[0].charge*s_tauEclean[0].charge < 0 :
+
+        e = get_TLorentzVector(s_electron[0])
+        tau = get_TLorentzVector(s_tauEclean[0])
+        jet = get_TLorentzVector(s_jet[0])
+
+        if ( jet.Pt() > 500 and ( isHT == 1 or isHTMHT == 1 ) ) or ( e.Pt() > 35 and isIsoEle == 1 ):
+
+            if pass_deltaR(e, tau, jet, 'ETau') == 1:
+
+                plot_variable('ETau', e, tau, jet, met)
+
+                # if len(s_isomuon) >= 2 : mumu_channel()
+                # if len(s_isomuon) >= 1 and len(s_isoelectron) >= 1 : emu_channel()
+                # if len(s_muon) >= 1 and len(s_tauMuclean) >= 1 : mutau_channel()
+
+out.cd()
+
+for key in h.keys():
+    h[key].Write()
+
+out.Close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
