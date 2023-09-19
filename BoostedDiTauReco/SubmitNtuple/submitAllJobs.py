62630f2cd11f2e44b7da1d54805d04fbc4e7a024
Muti <mw18h@my.fsu.edu>
2023-09-19 10:58:52 -0500

    Condor submission scripts

--- BoostedDiTauReco/SubmitNtuple/submitAllJobs.py
+++ BoostedDiTauReco/SubmitNtuple/submitAllJobs.py
@@ -3,13 +3,22 @@ import argparse
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="To submit MC/data to condor")
+    parser = argparse.ArgumentParser(description="To submit MC/data to condor. Example python3 submitAllJobs.py --mc DYJetsToLL_M-4to50")
     parser.add_argument("--complete", action="store_true", help="submit all MC samples and data (current default SingleE and JetHT)")
+    parser.add_argument("--sim", action="store_true", help="submit all MC samples only")
     parser.add_argument("--mc", nargs="+", type=str, help="MC samples to submit")
     parser.add_argument("--data", nargs="+", type=str, help="submit dataset")
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     os.system("./makeTarBall.csh")
+
+    if args.sim:
+#        SAMPLE = ['DYJetsToLL_M-50','DYJetsToLL_M-4to50','WJetsToLNu','WW','WZ','ZZ','ST_tW_top','ST_tW_antitop','ST_s-channel','ST_t-channel_antitop','ST_t-channel_top','QCD']
+
+        SAMPLE = ['WJetsToLNu', 'WW', 'ZZ', 'ST_tW_top','ST_tW_antitop','ST_s-channel','ST_t-channel_antitop', 'ST_t-channel_top']
+
+        for sample in SAMPLE:
+            os.system("./submitJobs.csh "+sample+" -b")
 
     if args.complete:
         SAMPLE = ['DYJetsToLL_M-50','DYJetsToLL_M-4to50','WJetsToLNu','WW','WZ','ZZ','ST_tW_top','ST_tW_antitop','TTTo2L2Nu','TTToSemiLeptonic','ST_s-channel','ST_t-channel_antitop','ST_t-channel_top','TTToHadronic','QCD']
